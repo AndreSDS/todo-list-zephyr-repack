f96045542716ed150df9a04fa6c982865f51aee8
André Souza da Silva <andre_smiths@outlook.com>
2026-01-23 12:42:01 -0300

    update config

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename);
  * Learn about Re.Pack configuration: https://re-pack.dev/docs/guides/configuration
  */
 
-//export default
+export default
 const config = Repack.defineRspackConfig(({ mode }) => ({
   mode,
   context: __dirname,
